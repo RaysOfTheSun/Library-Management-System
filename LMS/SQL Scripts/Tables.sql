0854ef21f7f4f3a1ae716e4c6e92b1745ef38b59
RaysOfTheSun <rdelsol35@gmail.com>
2018-05-19 00:04:26 +0800

    Update register page and SQL script(s)

--- LMS/SQL Scripts/Tables.sql
+++ LMS/SQL Scripts/Tables.sql
@@ -1,9 +1,9 @@
 ﻿IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_author')
+		AND TABLE_NAME = 'BookAuthors')
 	BEGIN
-		CREATE TABLE tbl_books (
+		CREATE TABLE BookAuthors (
 		authorID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 		firstName NVARCHAR(MAX) NULL,
 		middleName NVARCHAR(MAX) NULL,
@@ -13,9 +13,9 @@
 IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_publisher')
+		AND TABLE_NAME = 'BookPublishers')
 	BEGIN
-		CREATE TABLE tbl_publisher (
+		CREATE TABLE BookPublishers (
 		publisherID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 		publisherName NVARCHAR(MAX) NULL,
 		city NVARCHAR(MAX) NULL)
@@ -24,13 +24,13 @@ IF NOT EXISTS (
 IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_book')
+		AND TABLE_NAME = 'Books')
 	BEGIN
-		CREATE TABLE tbl_books (
+		CREATE TABLE Books (
 		bookID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 		title NVARCHAR(MAX) NULL,
-		authorID INT FOREIGN KEY REFERENCES author(authorID) NULL, 
-		publisherID INT FOREIGN KEY REFERENCES publisher(publisherID) NULL,
+		authorID INT FOREIGN KEY REFERENCES BookAuthors(authorID) NULL, 
+		publisherID INT FOREIGN KEY REFERENCES BookPublishers(publisherID) NULL,
 		publishYear SMALLINT NULL, 
 		genre NVARCHAR(MAX) NULL)
 	END
@@ -38,25 +38,24 @@ IF NOT EXISTS (
 IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_borrower')
+		AND TABLE_NAME = 'BookBorrowers')
 	BEGIN
-		CREATE TABLE tbl_borrower (
+		CREATE TABLE BookBorrowers (
 		borrowerID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 		firstName NVARCHAR(MAX) NULL,
 		middleName NVARCHAR(MAX) NULL,
 		lastName NVARCHAR(MAX) NULL,
-		mail NVARCHAR(MAX) NULL,
-		city NVARCHAR(MAX) NULL)
+		mail NVARCHAR(MAX) NULL)
 	END
 
 IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_account')
+		AND TABLE_NAME = 'UserAccounts')
 	BEGIN
-		CREATE TABLE tbl_account (
+		CREATE TABLE UserAccounts (
 		accountID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-		[owner] INT FOREIGN KEY REFERENCES tbl_borrower(borrowerID) NOT NULL,
+		[owner] INT FOREIGN KEY REFERENCES BookBorrowers(borrowerID) NOT NULL,
 		username NVARCHAR(MAX) NOT NULL,
 		[password] NVARCHAR(MAX) NOT NULL)
 	END
@@ -64,10 +63,46 @@ IF NOT EXISTS (
 IF NOT EXISTS ( 
 		SELECT * FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA = 'dbo'
-		AND TABLE_NAME = 'tbl_rental')
+		AND TABLE_NAME = 'BookRentals')
 	BEGIN
-		CREATE TABLE tbl_rental (
+		CREATE TABLE BookRentals (
 		rentalID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
 		bookID NVARCHAR(MAX) NOT NULL,
 		borrowerID NVARCHAR(MAX) NOT NULL)
 	END
+
+IF NOT EXISTS ( 
+		SELECT * FROM INFORMATION_SCHEMA.TABLES
+		WHERE TABLE_SCHEMA = 'dbo'
+		AND TABLE_NAME = 'BorrowerAddresses')
+	BEGIN
+		CREATE TABLE BorrowerAddresses (
+		adresslID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+		borrowerID INT FOREIGN KEY REFERENCES BookBorrowers(borrowerID) NOT NULL,
+		city NVARCHAR(MAX) NULL,
+		street NVARCHAR(MAX) NULL,
+		zipCode INT NULL)
+
+	END
+
+IF NOT EXISTS ( 
+		SELECT * FROM INFORMATION_SCHEMA.TABLES
+		WHERE TABLE_SCHEMA = 'dbo'
+		AND TABLE_NAME = 'BookStatuses')
+	BEGIN
+		CREATE TABLE BookStatuses (
+		rentalID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+		bookID NVARCHAR(MAX) NOT NULL,
+		borrowerID NVARCHAR(MAX) NOT NULL)
+	END
+
+
+--DROP TABLE UserAccounts, BookBorrowers
+--DROP TABLE BorrowerAddresses
+GO
+CREATE OR ALTER VIEW BorrowerAccounts AS
+SELECT b.firstName, b.middleName, b.lastName, a.city, a.street, a.zipCode, 
+	u.username, u.[password] FROM BookBorrowers b, UserAccounts u, BorrowerAddresses a
+
+--drop view BorrowerAccounts
+--select * from BorrowerAccounts
