eb49ce149ffedfeab7e0a3499d6198667d89ee4b
RaysOfTheSun <rdelsol35@gmail.com>
2018-05-19 15:16:48 +0800

    Update SQL script

--- LMS/SQL Scripts/Tables.sql
+++ LMS/SQL Scripts/Tables.sql
@@ -141,14 +141,38 @@ GO
 CREATE OR ALTER PROCEDURE CreateUser(@first NVARCHAR(MAX), @middle NVARCHAR(MAX), @last NVARCHAR(MAX), 
 	@mail NVARCHAR(MAX), @pass NVARCHAR(MAX), @login NVARCHAR(MAX), @country INT, @city NVARCHAR(MAX), @street NVARCHAR(MAX), @zip INT) AS
 	INSERT INTO BorrowerAddresses VALUES (@country, @city, @street, @zip)
-	INSERT INTO BookBorrowers(firstName,middleName,lastName,mail,addressID) VALUES (@first, @middle, @last, @mail, IDENT_CURRENT('BorrowerAdresses'))
+	INSERT INTO BookBorrowers(firstName,middleName,lastName,mail,addressID) VALUES (@first, @middle, @last, @mail, IDENT_CURRENT('BorrowerAddresses'))
 	INSERT INTO UserAccounts VALUES (IDENT_CURRENT('BookBorrowers'), @login, @pass)
 
+GO
 --EXEC CreateUser 'first', 'middle', 'last', 'mail@example.com', 'pass123', 'mail', 'new york', '42nd', '1037'
---select * from BookBorrowers
+select * from BookBorrowers
 --select * from UserAccounts
+----select * from BorrowerAddresses
+--select IDENT_CURRENT('BorrowerAddresses')
 
+
+--drop table BookBorrowers
 --drop view BorrowerAccounts
 --select * from BorrowerAccounts
 
+
+
+EXEC CreateUser 'Arvin', 'Collado', 'Alvarez', 'arvin@mail.com', '1111', 'rvn', 2, 1, 'Elm st.', 1008
+EXEC CreateUser 'John', 'Smith', 'Heather', 'john@mail.com', '21241', 'jsh', 2, 1, 'flem st.', 1010
+EXEC CreateUser 'Skratta', 'Purora', 'Mommin', 'scaty@mail.com', 'password', 'spm', 2, 1, 'lost st.', 1042
+GO
+
+SELECT * FROM BookBorrowers
+GO
+
+
 select * from Countries
+
+INSERT INTO Countries VALUES('Bhutan')
+INSERT INTO Countries VALUES('Philippines')
+INSERT INTO Countries VALUES('Brazil')
+INSERT INTO Countries VALUES('France')
+INSERT INTO Countries VALUES('Libia')
+INSERT INTO Countries VALUES('Philippines')
+INSERT INTO Countries VALUES('Germany')
