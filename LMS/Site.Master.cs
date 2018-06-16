b53723732ab6c2a0eb2fa7bc8e19e589daaa7210
RaysOfTheSun <rdelsol35@gmail.com>
2018-06-16 14:56:13 +0800

    Upadate master page search boxes
    + Search functions should also be invoked when clicking the appropriate search button in the header and footer of the user master page

--- LMS/Site.Master.cs
+++ LMS/Site.Master.cs
@@ -51,5 +51,15 @@ namespace LMS
             Session.Abandon();
             Response.Redirect("~/Home.aspx");
         }
+
+        protected void btn_bottomFind_Click(object sender, EventArgs e)
+        {
+            Response.Redirect($"~/Search.aspx?field=all&term={TbxBottomFind.Text}");
+        }
+
+        protected void BtnTopFind_Click(object sender, EventArgs e)
+        {
+            Response.Redirect($"~/Search.aspx?field=all&term={TbxTopFind.Text}");
+        }
     }
 }
